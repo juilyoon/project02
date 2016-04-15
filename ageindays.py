cb213a9a8f15d52987a497e116581bb84d16dbaf
Juil Yoon <jiy.juil@gmail.com>
2016-04-15 10:45:37 +0900

    Check isDateBefore()

--- ageindays.py
+++ ageindays.py
@@ -7,6 +7,8 @@
 # Problem:
 # Given your birthday and the current date, calculate your age in days. Compensate for leap days. Assume that the birthday and current date are correct dates (and no time travel). Simply put, if you were born 1 Jan 2012 and today's date is 2 jan 2012, you are 1 day old.
 
+#TODO:20 Write simple mechanical solution: Add days
+
 import datetime
 
 daysinMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -28,7 +30,20 @@ def getDays(day):
     else:
         return daysinMonth[day['m']-1] - day['d']
 
+def isDatesBefore(d1, d2):
+    '''Test if day 2 comes after day 1'''
+    if d1['y'] < d2['y']:
+        return True
+    elif d1['y'] == d2['y']:
+        if d1['m'] < d2['m']:
+            return True
+        elif d1['m'] = d2['m']:
+            return d1['d'] <= d2['d']
+    return False
+
 def daysBetween(d1, d2):
+    #DONE:20 Test for input validity and that d1 < d2
+    assert isDatesBefore(d1, d2)
     # Calculate days in full years only >>> number of days
     years, y = range(d1['y'], d2['y']), 0
     months = []
@@ -67,6 +82,7 @@ def daysBetween(d1, d2):
 def getBirthday():
     '''Receive input from user
     Return dict with birthday'''
+    # TODO:10 Test for input validity
     birthday = {'y': 1, 'm': 1, 'd': 1}
 
     print "When is your birthday?"
@@ -87,8 +103,24 @@ def main():
         print "Happy birthday!"
 
     print "You are", days, "days old!"
+
+def test():
+    test_cases = [((2012,1,1,2012,2,28), 58),
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
+
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+            print result
+        else:
+            print "Test case passed!"
+
 if __name__ == '__main__':
-    ###
+    ### #TODO:0 Move test cases to separate function
     print "Unit Tests"
     today = getDate()
     print "Today", today
